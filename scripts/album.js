0a20ba968f3517adf42c47af086b620d5da3edc6
K.Bailey <katebailey1230@gmail.com>
2016-11-17 12:57:11 -0500

    previous assignment

--- scripts/album.js
+++ scripts/album.js
@@ -5,11 +5,26 @@ var albumPicasso = {
      year: '1881',
      albumArtUrl: 'assets/images/album_covers/01.png',
      songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
+         { 
+             title: 'Blue', 
+             duration: '4:26' 
+         },
+         { 
+             title: 'Green', 
+             duration: '3:14' 
+         },
+         { 
+             title: 'Red', 
+             duration: '5:01' 
+         },
+         { 
+             title: 'Pink', 
+             duration: '3:21'
+         },
+         { 
+             title: 'Magenta', 
+             duration: '2:15'
+         }
      ]
  };
 var albumMarconi = {
@@ -19,11 +34,26 @@ var albumMarconi = {
      year: '1909',
      albumArtUrl: 'assets/images/album_covers/20.png',
      songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
+         { 
+             title: 'Hello, Operator?', 
+             duration: '1:01' 
+         },
+         { 
+             title: 'Ring, ring, ring', 
+             duration: '5:01' 
+         },
+         { 
+             title: 'Fits in your pocket', 
+             duration: '3:21'
+         },
+         { 
+             title: 'Can you hear me now?', 
+             duration: '3:14' 
+         },
+         { 
+             title: 'Wrong phone number', 
+             duration: '2:15'
+         }
      ]
  };
 var albumRedding = {
@@ -33,13 +63,29 @@ var albumRedding = {
      year: '1966',
      albumArtUrl: 'assets/images/album_covers/otis.jpg',
      songs: [
-         { title: 'Fa-Fa-Fa-Fa-Fa (Sad Song)', duration: '2:44' },
-         { title: 'I\'m sick Y\'all', duration: '2:57' },
-         { title: 'Tennessee Waltz', duration: '2:57'},
-         { title: 'Sweet Lorene', duration: '2:31' },
-         { title: 'Try a Little Tenderness', duration: '3:50'}
+         { 
+             title: 'Fa-Fa-Fa-Fa-Fa (Sad Song)', 
+             duration: '2:44' 
+         },
+         {
+             title: 'I\'m sick Y\'all', 
+             duration: '2:57'
+         },
+         { 
+             title: 'Tennessee Waltz', 
+             duration: '2:57'
+         },
+         { 
+             title: 'Sweet Lorene', 
+             duration: '2:31' 
+         },
+         {
+             title: 'Try a Little Tenderness', 
+             duration: '3:50'
+         }
      ]
  };
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -49,43 +95,50 @@ var createSongRow = function(songNumber, songName, songLength) {
       + '</tr>'
       ;
  
-     return template;
- };
+    return template;
+};
 
 
+var setCurrentAlbum = function(album) {
+    
 var albumTitle = document.getElementsByClassName('album-view-title')[0];
 var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
 var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-var setCurrentAlbum = function(album) {
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+albumTitle.firstChild.nodeValue = album.title;
+albumArtist.firstChild.nodeValue = album.artist;
+albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
-     albumSongList.innerHTML = '';
+     
+albumSongList.innerHTML = '';
  
      // #4
      for(var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
- };
+};
 
 
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
-     
+
+ 
+     var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albums = [albumPicasso, albumMarconi, albumRedding];
      var index = 1; 
-     
-     albumImage.addEventListener("click", function(event) { 
+
+window.onload = function() {
+    setCurrentAlbum(albumPicasso);
+    albumImage.addEventListener('click', function(event) { 
                 setCurrentAlbum(albums[index]); 
                 index++;
-                if(index == albums.length) {
+                if(index === albums.length) {
                     index = 0;
                 }
+        
  });
- };
+ }
+
+ 
+    
