81d596baeead0fb1139becbf2ad5ed6f1d64ec02
K.Bailey <katebailey1230@gmail.com>
2016-11-17 10:37:13 -0500

    image

--- scripts/album.js
+++ scripts/album.js
@@ -26,6 +26,20 @@ var albumMarconi = {
          { title: 'Wrong phone number', duration: '2:15'}
      ]
  };
+var albumRedding = {
+     title: 'Complete & Unbelievable: The Otis Redding Dictionary of Soul',
+     artist: 'Otis Redding',
+     label: 'Stax',
+     year: '1966',
+     albumArtUrl: 'assets/images/album_covers/otis.jpg',
+     songs: [
+         { title: 'Fa-Fa-Fa-Fa-Fa (Sad Song)', duration: '2:44' },
+         { title: 'I\'m sick Y\'all', duration: '2:57' },
+         { title: 'Tennessee Waltz', duration: '2:57'},
+         { title: 'Sweet Lorene', duration: '2:31' },
+         { title: 'Try a Little Tenderness', duration: '3:50'}
+     ]
+ };
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -37,15 +51,14 @@ var createSongRow = function(songNumber, songName, songLength) {
  
      return template;
  };
-var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
+
+var setCurrentAlbum = function(album) {
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -59,7 +72,19 @@ var setCurrentAlbum = function(album) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- 
+
+
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var album = [albumPicasso, albumMarconi, albumRedding];
+     var index = 1; 
+     
+     albumImage.addEventListener("click", function(event) { 
+                setCurrentAlbum(album[index]); 
+                index++;
+                if(index < album.length) {
+                    index = 0;
+                }
+ });
  };
