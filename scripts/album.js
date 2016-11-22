d7ad59ebf1311317e295fa05f7ab9142725aeb6a
K.Bailey <katebailey1230@gmail.com>
2016-11-22 11:17:42 -0500

    ignore.1

--- scripts/album.js
+++ scripts/album.js
@@ -28,8 +28,8 @@ var albumMarconi = {
  };
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
-        '<tr class="album-view-song-item">'
-       + '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+    '<tr class="album-view-song-item">'
+       +'<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -37,24 +37,21 @@ var createSongRow = function(songNumber, songName, songLength) {
  
      return template;
  };
-var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-
-     // #2
+var setCurrentAlbum = function(album) {
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+    
      albumSongList.innerHTML = '';
  
-     // #4
+ 
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -69,6 +66,16 @@ var currentlyPlayingSong = null;
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     var albums = [albumPicasso, albumMarconi];
+     var index = 1; 
+     
+     albumImage.addEventListener("click", function(event) { 
+                setCurrentAlbum(albums[index]); 
+                index++;
+                if(index == albums.length) {
+                    index = 0;
+                }
+ });
      
      var findParentByClassName = function(element, targetClass) {
     if (element) {
@@ -97,7 +104,7 @@ var currentlyPlayingSong = null;
     }  
 };
      var clickHandler = function(targetElement) {
-         var songItem = getSongItem(targetElement);  
+         var songItem = getSongItem(targetElement); 
          
          if (currentlyPlayingSong === null) {
          songItem.innerHTML = pauseButtonTemplate;
